d7233a4c739f699a6c7703bf8019e6bf13dfc444
minzhang <virgilerin@gmail.com>
2020-05-18 21:39:19 -0700

    updated solution 104

--- leetcode_solutions/104_Maximum_Depth_of_Binary_Tree.py
+++ leetcode_solutions/104_Maximum_Depth_of_Binary_Tree.py
@@ -1,16 +1,21 @@
-def max_cal(root:TreeNode, max_depth:int): 
-    depth_left,depth_right,depth_bottom = 0,0,0
-    if root.left is not None:
-        depth_left = max_cal(root.left, max_depth + 1)
-    if root.right is not None:
-        depth_right = max_cal(root.right, max_depth +1)
-    if root.right is None and root.left is None:
-        depth_bottom = max_depth +1
-    return_val = max(max(depth_left,depth_right),depth_bottom)
-    return return_val
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-if root is None:
-    return 0
-else:
-    max_depth = max_cal(root,0)
-    return(max_depth)
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        
+        if not root:
+            return 0
+        else:
+            def getMax(root, depth) -> int:
+                if not root:
+                    return depth
+                else:
+                    return max(getMax(root.left, depth + 1),getMax(root.right, depth + 1))
+            depth = getMax(root, 0)
+        return depth
+            
