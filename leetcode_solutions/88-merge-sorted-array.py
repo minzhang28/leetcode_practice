6b9a2a5f6c1f0c50d03530cabe9ab1bba8bf7a27
minzhang <virgilerin@gmail.com>
2020-07-01 23:00:59 -0700

    updated the solution for question 88

--- leetcode_solutions/88-merge-sorted-array.py
+++ leetcode_solutions/88-merge-sorted-array.py
@@ -6,8 +6,15 @@ class Solution:
         if not nums1 or not nums2:
             return None
         else:
-            print(m, n)
-            for i in range(n):
-                val = m + i 
-                nums1[val] = nums2[i]
-            nums1.sort()
+
+            p, p1, p2 = m+n-1, m-1, n-1
+            
+            while p1 >= 0 and p2 >= 0 :    
+                if nums1[p1] < nums2[p2]:
+                    nums1[p] = nums2[p2]
+                    p2 -= 1
+                else:
+                    nums1[p] =  nums1[p1]
+                    p1 -= 1
+                p -= 1
+            nums1[:p2 + 1] = nums2[:p2 + 1]
