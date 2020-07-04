cbff627776e6aeb24fcdf93e174ed744e7d9ce6c
minzhang <virgilerin@gmail.com>
2020-07-03 21:59:17 -0700

    added solution for question 167

--- leetcode_solutions/167-two-sum-ii-input-array-is-sorted.py
+++ leetcode_solutions/167-two-sum-ii-input-array-is-sorted.py
@@ -11,3 +11,23 @@ class Solution:
                     l = mid+1
                 else:
                     r = mid-1
+
+class Solution:
+    def twoSum(self, numbers, target):
+        
+        
+        if not numbers and not target:
+            return None
+        else:
+            
+            l = 0
+            r = len(numbers) - 1
+            
+            while l < r:
+                if numbers[l] + numbers[r] > target:
+                    r -= 1
+                elif numbers[l] + numbers[r] == target:
+                    return [l+1, r+1]
+                elif numbers[l] + numbers[r] < target:
+                    l += 1
+                                    
